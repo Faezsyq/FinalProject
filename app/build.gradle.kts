500da1a20bb83fdd92e40f2a0c575f8c620c46ba
Faezsyq <faez.syq@gmail.com>
2023-12-08 00:28:15 +0700

    tambahan ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("androidx.viewpager:viewpager:1.0.0")
 }
--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -3,15 +3,23 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewPager promoViewPager;
+    private int currentPage = 0;
 
+    private static final short DELAY_MS = 3000;
+    private static final short PERIOD_MS = 3000;
     List<FoodModel> elements;
 
     @Override
@@ -19,8 +27,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        promoViewPager = findViewById(R.id.promoViewPager);
+
+        // Gantilah dengan sumber daya gambar yang sesuai
+        int[] promoImages = {R.drawable.banner_discount_food, R.drawable.banner_discount_food, R.drawable.banner_discount_food};
+
+        PromoPagerAdapter promoPagerAdapter = new PromoPagerAdapter(this, promoImages);
+        promoViewPager.setAdapter(promoPagerAdapter);
+
+        // Otomatis ganti halaman setiap beberapa detik
+        final Handler handler = new Handler();
+        final Runnable update = new Runnable() {
+            public void run() {
+                if (currentPage == promoImages.length) {
+                    currentPage = 0;
+                }
+                promoViewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+        Timer swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        }, DELAY_MS, PERIOD_MS);
         addData();
     }
+//        int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.item_spacing);
+
 
     private void addData() {
         elements = new ArrayList<>();
@@ -46,7 +82,9 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
         recyclerView.setNestedScrollingEnabled(false);
+        recyclerView.addItemDecoration(new RecyclerViewItemDecoration(this));
     }
+
 
 //    private void detail(FoodModel item) {
 //        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
